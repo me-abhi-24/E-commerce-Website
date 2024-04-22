489db71218ebc05ee3b0d6b9c5df1fda2013ed22
Abhishek Kumar <125692870+me-abhi-24@users.noreply.github.com>
2024-04-22 14:30:20 +0000

    CompleteFrontend

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="decriptionbox-nav-box">Description</div>
+        <div className="decriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima incidunt dolore voluptas. Aut ab quae porro accusantium? Commodi cumque perferendis iste debitis, officiis nulla assumenda adipisci, incidunt molestias vero hic?</p>
+      </div>
+    </div>
+  )
+}
+export default DescriptionBox
